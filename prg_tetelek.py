bb2cf99ef404e098b578064faf191fee143af559
AnchaToth <ancha.tot@gmail.com>
2022-03-28 13:37:12 +0200

    @13:36

--- prg_tetelek.py
+++ prg_tetelek.py
@@ -7,6 +7,8 @@ def sum_numbers(list_of_numbers):  ##  ne használjunk változó névben típus 
         result += num
     return result
 
+
+## Számolás tétele
 # Írjatok egy olyan függvényt, ami paraméterül kap
 # egy stringet és visszaadja, hogy hány magyar ékezetes
 # karakter tartalmaz!
@@ -29,6 +31,42 @@ def count_accented_new(word):
             counter += 1
     return counter
 
+## szélsőérték keresés
+# írj egy függvényt ami visszaadja mai visszadja a leghosszabb szót
+# a paraméterként átadott szövegből. A szövegben a szavakat space-ek 
+# választják el egymástól
+
+def longest_word(text):
+    words = text.split()
+    longest = ""
+    for word in words:
+        if len(word) > len(longest):
+            longest = word
+    
+    return longest
+
+def find_longest_word(text):   ##  oktatói változat
+    max_length = 0
+    for word in text.split():
+        actual_length = len(word)
+        if actual_length > max_length:
+            max_length = actual_length
+            result = word       ##  üres stringnél hibát dob, mivel a result csak a for alatt van deklarálva
+    return result
+
+
+## Eldöntés tétele
+# írj egy fv-t, amely eldönti egy listáról, hogy csak
+# pozitív számokat tartalmaz-e! Ha egy 0 vagy negatív szám is
+# van benn, akkor térjen vissza False értékkel!
+
+def check_for_natural_numbers(numbers: list[int]) -> bool:
+    for num in numbers:
+        if num <= 0:
+            return False
+    return True
+
+
 #print("hello modul")
 #print(f"a __name___ valtozo erteke: {__name__}")
 
@@ -40,4 +78,5 @@ if __name__ == "__main__":
     print(count_accented("bármicsakszöveglegyen"))
 
     print(count_accented_new("akármiÉkezetes"))
-    
+
+    print(longest_word("valami szöveg csak úgy"))
--- prg_tetelek_test.py
+++ prg_tetelek_test.py
@@ -1,4 +1,4 @@
-from prg_tetelek import count_accented, count_accented_new, sum_numbers 
+from prg_tetelek import count_accented, count_accented_new, longest_word, sum_numbers, check_for_natural_numbers
 
 def test_sum():
     #given
@@ -17,3 +17,15 @@ def test_count_accented():
 def test_count_accented_2():
     assert count_accented_new("akármicsakÉkezetesbetű") == 3
     assert count_accented_new("elégMÁRazÉkezetekből") == 4
+
+def test_longest_word():
+    assert longest_word("valamit írok ide csak hogy teszteljek") == "teszteljek"
+
+def test_check_for_natural_numbers():
+    assert check_for_natural_numbers([3,7,5,2,1])
+
+def test_check_for_natural_numbers_negativ():
+    assert not check_for_natural_numbers([3,7,5,-2,1])
+
+def test_check_for_natural_numbers_null():
+    assert not check_for_natural_numbers([3,7,5,0,1])
